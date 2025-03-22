adc54747d40b1400764f15484d12118534adf057
Najam Iftikhar <dev.najam@gmail.com>
2025-03-22 08:23:07 +0500

    update

--- src/components/atoms/button/button.tsx
+++ src/components/atoms/button/button.tsx
@@ -2,12 +2,5 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
 export const Button: React.FC<ButtonProps> = ({ children, ...props }) => {
-  return (
-    <button
-      className='px-6 font-light py-3 rounded-full  text-white bg-[#0071E3] hover:bg-[#0071E3]/95'
-      {...props}
-    >
-      {children}
-    </button>
-  );
+  return <Button {...props}>{children}</Button>;
 };
